6e06e8c067c243cdf7518e1936b5635750f13015
Christian Boelsen <christian.boelsen+github@gmail.com>
2017-09-22 08:40:35 +0100

    Start nginx when running the web app in docker.

--- files/run_server_dev.sh
+++ files/run_server_dev.sh
@@ -4,4 +4,6 @@ test-chest migrate --noinput --settings test_chest_project.test_chest_project.se
 
 test-chest shell -c "from django.contrib.auth.models import User; u, _ = User.objects.get_or_create(username='test'); u.set_password('test'); u.is_staff = True; u.save()" --settings test_chest_project.test_chest_project.settings.dev
 
+nginx &
+
 supervisord -c /etc/supervisor/supervisord.conf
